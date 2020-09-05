309a7406fcd780298c22c3f5d1011362da9076bc
David Silva Jr <david_sjr@hotmail.com>
2020-09-05 19:39:30 -0300

    Ajustes

---
+++ backend/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Model.Autenticacao;
+using Service.Interfaces;
+
+namespace WebApi.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ITokenService<User> _tokenService;
+        private readonly ICarregaService<User> _carregaService;
+        private readonly IPersistenciaService _persistenciaService;
+        public HomeController(ITokenService<User> tokenService, ICarregaService<User> carregaService, IPersistenciaService persistenciaService)
+        {
+            _tokenService = tokenService;
+            _carregaService = carregaService;
+            _persistenciaService = persistenciaService;
+        }
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult<dynamic> Autenticacao(User model)
+        {
+            model.Password = _tokenService.Encrypt(model.Password);
+            var user = _carregaService.CarregaPor(model);
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = _tokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+        [HttpPost]
+        [Route("registrar")]
+        [Authorize]
+        public ActionResult<dynamic> Registrar(User model)
+        {
+            try
+            {
+                model.Password = _tokenService.Encrypt(model.Password);
+                var user = _carregaService.CarregaPor(model);
+                if (user == null)
+                {
+                    if (_persistenciaService.Add(model))
+                    {
+                        model.Password = "";
+                        return Ok(model);
+                    }
+                    return BadRequest("Erro ao salvar: ");
+                }
+                return BadRequest("Já existe");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro ao acessar banco: {e.Message}");
+            }
+        }
+
+        [HttpPut]
+        [Route("atualizar")]
+        [Authorize]
+        [Authorize(Roles = "admin")]
+        public ActionResult<dynamic> Atualizar(User model)
+        {
+            try
+            {
+                model.Password = _tokenService.Encrypt(model.Password);
+                if (_persistenciaService.Update(model))
+                {
+                    model.Password = "";
+                    return Ok(model);
+                }
+                return BadRequest("Erro ao salvar: ");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro ao acessar banco: {e.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => $"Autenticado - {User.Identity.Name}";
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee, manager")]
+        public string Employee() => $"Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => $"Gerente";
+
+
+    }
+}
--- backend/WebAPI/Program.cs
+++ backend/WebAPI/Program.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using WebApi;
 
-namespace WebApi
+namespace WebAPI
 {
     public class Program
     {
